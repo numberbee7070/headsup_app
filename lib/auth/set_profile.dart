11b0aa04dd7de077a5d2dfc989a3ae2bf82452c6
Yash Garg <yash.garg.cd.ece19@itbhu.ac.in>
2020-11-04 11:26:20 +0530

    error handling and profile create page

---
+++ lib/auth/set_profile.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import '../model/http_exceptions.dart';
+import '../ui/text_fields.dart';
+import 'services/service.dart';
+
+class SetProfile extends StatefulWidget {
+  @override
+  _SetProfileState createState() => _SetProfileState();
+}
+
+class _SetProfileState extends State<SetProfile> {
+  TextEditingController usernameController = TextEditingController();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  void dispose() {
+    usernameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("enter a username for your account"),
+              AuthTextField(
+                controller: usernameController,
+                prefixIcon: Icon(Icons.supervised_user_circle_outlined),
+                hintText: AutofillHints.newUsername,
+              ),
+              SizedBox(height: 20.0),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color: Colors.pink[200],
+                onPressed: () async {
+                  this._scaffoldKey.currentState.showSnackBar(
+                      SnackBar(content: Text("Updating profile")));
+                  try {
+                    await AuthServices.newSignUp(this.usernameController.text);
+                  } on SocketException {
+                    this._scaffoldKey.currentState.showSnackBar(SnackBar(
+                        content: Text("Network error. Try again later")));
+                    return;
+                  } on HttpServerError {
+                    this._scaffoldKey.currentState.showSnackBar(SnackBar(
+                        content: Text("Server error. Try again later")));
+                    return;
+                  }
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  "Continue",
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,8 @@
-import 'dart:ui';
-
+import 'package:app/auth/set_profile.dart';
 import 'package:flutter/material.dart';
 
+import '../auth/services/service.dart';
+import '../model/http_exceptions.dart';
 import '../ui/hamburger_menu.dart';
 import 'diary.dart';
 import 'reads.dart';
@@ -13,43 +14,103 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Size size;
+  Future _future;
+
+  @override
+  void initState() {
+    super.initState();
+    _future = loadUser();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        body: SafeArea(
-          child: Stack(
-            children: [
-              TabBarView(
-                children: [
-                  Reads(),
-                  Diary(),
-                ],
+    return FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+                color: Colors.white,
+                child: Center(child: CircularProgressIndicator()));
+          }
+          if (snapshot.hasError) {
+            print("homepage: ${snapshot.error}");
+          }
+          return DefaultTabController(
+            length: 2,
+            child: Scaffold(
+              extendBody: true,
+              body: SafeArea(
+                bottom: false,
+                child: Stack(
+                  children: [
+                    snapshot.hasError
+                        ? errorWidget()
+                        : TabBarView(
+                            children: [
+                              Reads(),
+                              Diary(),
+                            ],
+                          ),
+                    HamburgerMenu(),
+                  ],
+                ),
               ),
-              HamburgerMenu(),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.book),
+              bottomNavigationBar: BottomAppBar(
+                shape: CircularNotchedRectangle(),
+                child: TabBar(
+                  labelColor: Colors.green,
+                  unselectedLabelColor: Colors.red,
+                  tabs: [
+                    Tab(
+                      icon: Icon(
+                        Icons.book,
+                        // color: Colors.red,
+                      ),
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.bookmark,
+                        // color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              Tab(
-                icon: Icon(Icons.bookmark),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.gamepad_rounded),
               ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.gamepad_rounded),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      ),
-    );
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.centerDocked,
+            ),
+          );
+        });
   }
+
+  Future loadUser() async {
+    /// if user does not exist on backend
+    /// ask user to create profile
+    print("fetching user details");
+    try {
+      await AuthServices.fetchUserDetails();
+    } on HttpForbidden {
+      print("user does not exist. create profile");
+      await Navigator.push(
+          context, MaterialPageRoute(builder: (context) => SetProfile()));
+    }
+  }
+
+  Widget errorWidget() => Container(
+        child: Center(
+          child: Column(
+            children: [
+              Text("Error connecting"),
+              FlatButton(
+                onPressed: () => this.setState(() => this._future = loadUser()),
+                child: Text("Tap to try again"),
+              ),
+            ],
+          ),
+        ),
+      );
 }
