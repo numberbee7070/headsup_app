e050c8fa9e8e8a190b39ea5ec682b9518bc67e89
Yash Garg <yash.garg.cd.ece19@itbhu.ac.in>
2020-11-08 18:35:09 +0530

    animated icon size

--- lib/pages/article.dart
+++ lib/pages/article.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:shimmer/shimmer.dart';
@@ -20,6 +23,8 @@ class _ArticlePageState extends State<ArticlePage>
   final player = AudioPlayer();
   AnimationController _animation;
   bool _enablePlayback = false;
+  double _iconSize = 50.0;
+  StreamController<double> _iconSizeStream;
 
   @override
   void initState() {
@@ -29,171 +34,192 @@ class _ArticlePageState extends State<ArticlePage>
     player.playerStateStream.listen(playerStateListener);
     _future = getArticle(widget.articleIdx);
     _future.then((Article value) => initialiseAudio(value.audio));
+    _iconSizeStream = StreamController<double>();
   }
 
   @override
   void dispose() {
-    player.dispose();
+    player.dispose().then((_) => _animation.dispose());
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            floating: false,
-            pinned: true,
-            snap: false,
-            expandedHeight: 200.0,
-            flexibleSpace: Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                FlexibleSpaceBar(
-                  background: FutureBuilder<Article>(
-                    future: _future,
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        if (snapshot.hasData) {
-                          return Stack(
-                            fit: StackFit.expand,
-                            children: [
-                              Image.network(
-                                snapshot.data.image,
-                                fit: BoxFit.cover,
-                              ),
-                              Container(color: Colors.black45),
-                            ],
-                          );
-                        } else {
-                          return Text("Error occured");
-                        }
-                      } else {
-                        return Container();
-                      }
-                    },
-                  ),
-                ),
-                Padding(
-                  padding:
-                      EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-                  child: _enablePlayback
-                      ? Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            IconButton(
-                                icon: Icon(Icons.replay_10),
-                                onPressed: () {
-                                  var newpos =
-                                      player.position + Duration(seconds: -10);
-                                  player.seek(newpos.isNegative
-                                      ? Duration.zero
-                                      : newpos);
-                                }),
-                            IconButton(
-                                icon: AnimatedIcon(
-                                  icon: AnimatedIcons.play_pause,
-                                  progress: _animation,
-                                  size: 30,
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification scrollinfo) {
+          if (scrollinfo.metrics.pixels < 50)
+            _iconSizeStream
+                .add(max(30.0, 50 - 0.5 * scrollinfo.metrics.pixels));
+          return true;
+        },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: false,
+              pinned: true,
+              snap: false,
+              expandedHeight: 200.0,
+              flexibleSpace: Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  FlexibleSpaceBar(
+                    background: FutureBuilder<Article>(
+                      future: _future,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          if (snapshot.hasData) {
+                            return Stack(
+                              fit: StackFit.expand,
+                              children: [
+                                Image.network(
+                                  snapshot.data.image,
+                                  fit: BoxFit.cover,
                                 ),
-                                onPressed: () {
-                                  print("play");
-                                  if (player.playing) {
-                                    player.pause();
-                                    _animation.reverse();
-                                  } else {
-                                    player.play();
-                                    _animation.forward();
-                                  }
-                                }),
-                            IconButton(
-                              icon: Icon(Icons.forward_10),
-                              onPressed: () {
-                                var newpos =
-                                    player.position + Duration(seconds: 10);
-                                player.seek(newpos > player.duration
-                                    ? player.duration
-                                    : newpos);
-                              },
-                            )
-                          ],
-                        )
-                      : CircularProgressIndicator(
-                          valueColor:
-                              AlwaysStoppedAnimation<Color>(Colors.green),
-                        ),
-                ),
-                Positioned(
-                  right: 10.0,
-                  bottom: 10.0,
-                  child: StreamBuilder<Duration>(
-                    stream: player.positionStream,
-                    builder: (context, snapshot) {
-                      var position = snapshot.data ?? Duration.zero;
-                      return Text(
-                          "${position.toString().split('.')[0]} | ${player.duration.toString().split('.')[0]}");
-                    },
+                                Container(color: Colors.black45),
+                              ],
+                            );
+                          } else {
+                            return Text("Error occured");
+                          }
+                        } else {
+                          return Container();
+                        }
+                      },
+                    ),
                   ),
-                )
-              ],
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              FutureBuilder<Article>(
-                future: _future,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    if (snapshot.hasData) {
-                      return Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(
-                          snapshot.data.title,
-                          style: TextStyle(fontSize: 30.0),
-                          textAlign: TextAlign.center,
-                        ),
-                      );
-                    } else {
-                      return Text("Error occured");
-                    }
-                  } else {
-                    return Container();
-                  }
-                },
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).padding.top),
+                    child: _enablePlayback
+                        ? StreamBuilder<double>(
+                            initialData: _iconSize,
+                            stream: _iconSizeStream.stream,
+                            builder: (context, snapshot) => Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                IconButton(
+                                    iconSize: snapshot.data,
+                                    icon: Icon(
+                                      Icons.replay_10,
+                                      color: Colors.white,
+                                    ),
+                                    onPressed: () {
+                                      var newpos = player.position +
+                                          Duration(seconds: -10);
+                                      player.seek(newpos.isNegative
+                                          ? Duration.zero
+                                          : newpos);
+                                    }),
+                                IconButton(
+                                    iconSize: snapshot.data,
+                                    icon: AnimatedIcon(
+                                      icon: AnimatedIcons.play_pause,
+                                      progress: _animation,
+                                      color: Colors.white,
+                                    ),
+                                    onPressed: () {
+                                      if (player.playing) {
+                                        player.pause();
+                                        _animation.reverse();
+                                      } else {
+                                        player.play();
+                                        _animation.forward();
+                                      }
+                                    }),
+                                IconButton(
+                                  iconSize: snapshot.data,
+                                  icon: Icon(
+                                    Icons.forward_10,
+                                    color: Colors.white,
+                                  ),
+                                  onPressed: () {
+                                    var newpos =
+                                        player.position + Duration(seconds: 10);
+                                    player.seek(newpos > player.duration
+                                        ? player.duration
+                                        : newpos);
+                                  },
+                                )
+                              ],
+                            ),
+                          )
+                        : CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.green),
+                          ),
+                  ),
+                  Positioned(
+                    right: 10.0,
+                    bottom: 10.0,
+                    child: StreamBuilder<Duration>(
+                      stream: player.positionStream,
+                      builder: (context, snapshot) {
+                        var position = snapshot.data ?? Duration.zero;
+                        return Text(
+                            "${position.toString().split('.')[0]} | ${player.duration.toString().split('.')[0]}");
+                      },
+                    ),
+                  )
+                ],
               ),
-              FutureBuilder<Article>(
-                future: _future,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    if (snapshot.hasData) {
-                      return Center(
-                        child: Padding(
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                FutureBuilder<Article>(
+                  future: _future,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        return Padding(
                           padding: const EdgeInsets.all(20.0),
                           child: Text(
-                            snapshot.data.body,
-                            style: TextStyle(
-                              letterSpacing: 1.2,
-                              height: 1.5,
-                            ),
-                            textAlign: TextAlign.justify,
+                            snapshot.data.title,
+                            style: TextStyle(fontSize: 30.0),
+                            textAlign: TextAlign.center,
                           ),
-                        ),
-                      );
+                        );
+                      } else {
+                        return Text("Error occured");
+                      }
                     } else {
-                      return Text("Error occured");
+                      return Container();
                     }
-                  } else {
-                    return Shimmer.fromColors(
-                        child: shimmerWidget,
-                        baseColor: Colors.grey,
-                        highlightColor: Colors.white60);
-                  }
-                },
-              ),
-            ]),
-          )
-        ],
+                  },
+                ),
+                FutureBuilder<Article>(
+                  future: _future,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        return Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Text(
+                              snapshot.data.body,
+                              style: TextStyle(
+                                letterSpacing: 1.2,
+                                height: 1.5,
+                              ),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ),
+                        );
+                      } else {
+                        return Text("Error occured");
+                      }
+                    } else {
+                      return Shimmer.fromColors(
+                          child: shimmerWidget,
+                          baseColor: Colors.grey,
+                          highlightColor: Colors.white60);
+                    }
+                  },
+                ),
+              ]),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -224,6 +250,7 @@ class _ArticlePageState extends State<ArticlePage>
         break;
       case ProcessingState.completed:
         _animation.reverse();
+        player.stop();
         break;
       case ProcessingState.none:
         _enablePlayback = false;
