23005c4ba65dfa93768d3dd304de38c138eb834c
Yash Garg <yash.garg.cd.ece19@itbhu.ac.in>
2020-11-18 01:13:22 +0530

    welcome page image size

--- lib/welcome/welcome.dart
+++ lib/welcome/welcome.dart
@@ -1,37 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+
 import '../auth/auth.dart';
 
-PageViewModel page1 = PageViewModel(
-  title: "PLAY & RELAX",
-  image: Image.asset("assets/images/welcome_page1.png"),
-  body: "Lorem ipsum dolor sit amet",
-  decoration: PageDecoration(imageFlex: 3),
-);
-
-PageViewModel page2 = PageViewModel(
-  title: "CONNECT WITH MATES",
-  image: Image.asset("assets/images/welcome_page2.png"),
-  body: "Lorem ipsum dolor sit amet",
-  decoration: PageDecoration(imageFlex: 3),
-);
-
-PageViewModel page3 = PageViewModel(
-  title: "TRUSTED EXPERTS",
-  image: Image.asset("assets/images/welcome_page3.png"),
-  body: "Lorem ipsum dolor sit amet",
-  decoration: PageDecoration(imageFlex: 3),
-);
-
-class IntroScreen extends StatefulWidget {
+class IntroScreen extends StatelessWidget {
   static String routeName = "intro";
   @override
-  _IntroScreenState createState() => _IntroScreenState();
-}
-
-class _IntroScreenState extends State<IntroScreen> {
-  @override
   Widget build(BuildContext context) {
+    final pageDecoration = PageDecoration(
+      imageFlex: 3,
+      imagePadding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05),
+    );
+
+    PageViewModel page1 = PageViewModel(
+      title: "PLAY & RELAX",
+      image: Image.asset("assets/images/welcome_page1.png"),
+      body: "Lorem ipsum dolor sit amet",
+      decoration: pageDecoration,
+    );
+
+    PageViewModel page2 = PageViewModel(
+      title: "CONNECT WITH MATES",
+      image: Image.asset("assets/images/welcome_page2.png"),
+      body: "Lorem ipsum dolor sit amet",
+      decoration: pageDecoration,
+    );
+
+    PageViewModel page3 = PageViewModel(
+      title: "TRUSTED EXPERTS",
+      image: Image.asset("assets/images/welcome_page3.png"),
+      body: "Lorem ipsum dolor sit amet",
+      decoration: pageDecoration,
+    );
+
     return IntroductionScreen(
       pages: <PageViewModel>[page1, page2, page3],
       done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
